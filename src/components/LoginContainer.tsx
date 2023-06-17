51ded25867464611b9bb47f5efa24af0391c6fc8
George Vazquez <george.h.vazquez@gmail.com>
2023-06-16 22:21:52 -0400

    login page styling start

--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useUserStore from "../store/userStore";
+import { useStore } from "zustand";
+import { TextField, Button } from "@mui/material";
+
 
 const LoginContainer = () => {
+  const { name, email, isLoggedIn, setName, setEmail, setLoggedIn } = useStore(useUserStore);
+
+  const handleNameChange = (e: string) => {
+    setName(e);
+  };
+
+  const handleEmailChange = (e: string) => {
+    setEmail(e);
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    // check if valid e-mail. receive 400 status if @ or .com not in email entry but get access token anyway
+    try {
+      const fetchResponse = await fetch('https://frontend-take-home-service.fetch.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "name": name,
+          "email": email
+      }),
+        credentials: 'include',
+      });
+      const status = await fetchResponse.status;
+      if (status === 200 ){
+        setLoggedIn(true);
+        navigate('/main');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return(
     <>
-      <p>This is the login container</p>
+      <div className="login-container">
+        <div className="form-container">
+          <p className="body-h4">Welcome!</p>
+          <input type="text" placeholder="Name" onChange={(e) => handleNameChange(e.target.value)} />
+          <input type="text" placeholder="E-mail" onChange={(e) => handleEmailChange(e.target.value)} />
+          <Button variant='contained' className='button-primary-form' onClick={() => handleSubmit()}>Let's go!</Button>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,51 +1,10 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import useUserStore from "../store/userStore";
-import { useStore } from "zustand";
+import React from "react";
+import LoginContainer from "../components/LoginContainer";
 
 const Login = () => {
-  const { name, email, isLoggedIn, setName, setEmail, setLoggedIn } = useStore(useUserStore);
-
-  const handleNameChange = (e: string) => {
-    setName(e);
-  };
-
-  const handleEmailChange = (e: string) => {
-    setEmail(e);
-  };
-
-  const navigate = useNavigate();
-
-  const handleSubmit = async () => {
-    // check if valid e-mail. receive 400 status if @ or .com not in email entry but get access token anyway
-    try {
-      const fetchResponse = await fetch('https://frontend-take-home-service.fetch.com/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          "name": name,
-          "email": email
-      }),
-        credentials: 'include',
-      });
-      const status = await fetchResponse.status;
-      if (status === 200 ){
-        setLoggedIn(true);
-        navigate('/main');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return(
     <>
-      <p>Login</p>
-      <input type="text" placeholder="Name" onChange={(e) => handleNameChange(e.target.value)} />
-      <input type="text" placeholder="E-mail" onChange={(e) => handleEmailChange(e.target.value)} />
-      <button onClick={() => handleSubmit()}>Submit</button>
+      <LoginContainer />
     </>
   );
 };
