0810b049f3d2f76d2ac3283ac1306bd4e6adcc6c
George Vazquez <george.h.vazquez@gmail.com>
2023-07-01 22:02:38 -0400

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -6,59 +6,62 @@ const { DefinePlugin } = require('webpack');
 
 const dotenv = require('dotenv').config({ path: __dirname + '/.env' })
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  mode: 'development',
-  devServer: {
-    host: 'localhost',
-    port: 8080,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, './build'),
+module.exports = (env) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: 'bundle.js',
       publicPath: '/',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        exclude: /(node_modues)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+    mode: 'development',
+    devServer: {
+      host: 'localhost',
+      port: 8080,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, './build'),
+        publicPath: '/',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /(node_modues)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /(node_modues)/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /(node_modules)/,
-        use: ['ts-loader'],
-      },
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /(node_modues)/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /(node_modules)/,
+          use: ['ts-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './index.html',
+      }),
+      new DefinePlugin({
+        'process.env': JSON.stringify(dotenv.parsed),
+        'process.env.REACT_APP_ENV': env.REACT_APP_ENV === 'gh' ? JSON.stringify('gh') : JSON.stringify(dotenv.parsed.REACT_APP_ENV),
+      })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './index.html',
-    }),
-    new DefinePlugin({
-      'process.env': JSON.stringify(dotenv.parsed),
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+  };
 };
