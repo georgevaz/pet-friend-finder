7a854f24573f33c422a48749710b1d95b1e9fe0e
George Vazquez <george.h.vazquez@gmail.com>
2023-07-03 19:14:18 -0400

    testing suite setup

--- __tests__/dogStore.test.ts
+++ __tests__/dogStore.test.ts
@@ -1,21 +1,33 @@
 import useDogStore from '../src/store/dogStore';
 
-// TODO
 // mock stores with Zustand/Jest brings up issues and errors involving "create" functionality within Zustand itself
-// utilizing getState with the stores for now
+// utilizing getState
 
 describe('Testing dogStore', () => {
   const mock = useDogStore.getState();
-  const { addFavoriteDog } = mock;
-
-  test('should add ID to empty state', () => {
+  const { resetZips, addFavoriteDog, removeFavoriteDog } = mock;
+  test('reset all zips from filter', () => {
+    resetZips();
+    expect(useDogStore.getState().zips).toHaveLength(0);
+  });
+  test('add ID to favorites', () => {
     addFavoriteDog('NXGFTIcBOvEgQ5OCx8A1');
-    expect(useDogStore.getState().favoriteDogsIds).toStrictEqual([
+    expect(useDogStore.getState().favoriteDogsIds).toEqual([
       'NXGFTIcBOvEgQ5OCx8A1',
     ]);
+    addFavoriteDog('RXGFTIcBOvEgQ5OCx8A1');
+    expect(useDogStore.getState().favoriteDogsIds).toEqual([
+      'NXGFTIcBOvEgQ5OCx8A1',
+      'RXGFTIcBOvEgQ5OCx8A1',
+    ]);
   });
-
-  test('testing', () => {
-    expect(true).toBe(true);
+  test('remove ID from favorites', () => {
+    useDogStore.setState({
+      favoriteDogsIds: ['NXGFTIcBOvEgQ5OCx8A1', 'RXGFTIcBOvEgQ5OCx8A1'],
+    });
+    removeFavoriteDog('NXGFTIcBOvEgQ5OCx8A1');
+    expect(useDogStore.getState().favoriteDogsIds).toEqual([
+      'RXGFTIcBOvEgQ5OCx8A1',
+    ]);
   });
 });
